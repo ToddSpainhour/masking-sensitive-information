bde665418ad299745a2d5aa8d216c306f5ea4036
Todd Spainhour <toddspainhour@gmail.com>
2020-08-02 16:18:21 -0500

    Made another example that works with entries of unknown length

--- masking-sensitive-information/Program.cs
+++ masking-sensitive-information/Program.cs
@@ -7,6 +7,68 @@ namespace masking_sensitive_information
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Enter a secret and we'll conceal it while only showing the last four digits.");
+            Console.WriteLine("\n");
+
+
+
+            var usersSecret = Console.ReadLine();
+            Console.WriteLine("\n");
+
+
+
+            Console.WriteLine($"You typed '{usersSecret}. Oh no. It's not concealed. How about this?'");
+            Console.WriteLine("\n");
+
+
+
+            var lengthOfSecret = usersSecret.Length;
+
+
+
+            var lengthOfSecretMinusFour = usersSecret.Length - 4;
+
+
+
+            string redacted = new string('*', lengthOfSecretMinusFour);
+
+
+
+            Console.WriteLine($"the 'redacted' variable returns '{redacted}'");
+            Console.WriteLine("\n");
+
+
+
+            var lastFourOfSecret = usersSecret.Substring(usersSecret.Length - 4, 4);
+
+
+
+            Console.WriteLine($"The variable 'lastFourOfSecret' returns '{lastFourOfSecret}'");
+            Console.WriteLine("\n");
+
+
+
+            string redactedAndExposedCombined = redacted + lastFourOfSecret;
+
+
+
+            Console.WriteLine($"When we recombine both of those variables we get {redactedAndExposedCombined}");
+            Console.WriteLine("\n");
+
+
+
+
+            // how do i print a certain number of * to replace the first part of the secrete while exposing the last four parts using the variable above
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
             Console.WriteLine("Let's learn about masking information.");
                     Console.WriteLine("\n");
 
